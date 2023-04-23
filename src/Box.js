7d71998bd0d3fa42c92872a526e68ec67d856f34
hail000 <hj.hail000@gmail.com>
2023-04-23 21:41:57 +0900

    Add files via upload

---
+++ src/Box.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Box = (props) => {
+  console.log("props", props);
+  const result =
+    props.use === "컴퓨터"
+      ? props.result === "이겼습니다!"
+        ? "졌습니다!"
+        : props.result === "졌습니다!"
+        ? "이겼습니다!"
+        : props.result
+      : props.result;
+  return (
+    <div className="Box">
+      <h1>{props.use}</h1>
+      <h1>{props.item && props.item.name}</h1>
+      <img className="item-img" src={props.item && props.item.img}></img>
+      <h1>{result}</h1>
+    </div>
+  );
+};
+
+export default Box;
